c703c83715504e3e74c26b647c2bc403f1e308d0
Miquéias Franco <121680934+MiqueiasFranco@users.noreply.github.com>
2023-10-19 12:28:10 -0300

    components dinamics properts

--- src/app/components/smallcard/smallcard.component.ts
+++ src/app/components/smallcard/smallcard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-smallcard',
   templateUrl: './smallcard.component.html',
   styleUrls: ['./smallcard.component.css']
 })
-export class SmallcardComponent {
-
+export class SmallcardComponent implements OnInit {
+  @Input()
+  photoCover:string=""
+  @Input()
+  cardTitle:string=""
+  
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
 }
